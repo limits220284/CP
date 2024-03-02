da8c9a8607916646d0763573c3ed051741fcd69a
limits220284 <2056901285@qq.com>
2024-03-02 20:13:47 +0800

    add leetcode solutions

--- leetcode/1.两数之和.go
+++ leetcode/1.两数之和.go
@@ -1,10 +1,11 @@
-func twoSum(nums []int, target int) []int {
-	hh := make(map[int]int)
-	for i, v := range nums {
-		if idx, ok := hh[target - v]; ok {
-			return []int{idx, i}
-		}
-		hh[v] = i
-	}
-	return []int{}
+func twoSum(nums []int, target int) []int {
+
+	hh := make(map[int]int)
+	for i, v := range nums {
+		if idx, ok := hh[target - v]; ok {
+			return []int{idx, i}
+		}
+		hh[v] = i
+	}
+	return []int{}
 }
